16af1bbccf9b6d8e9fbbbad1df8028b894826152
pnv13 <ivanpakhotnykh@mail.ru>
2022-01-01 13:09:27 +0300

    feat: complete arrays and data

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -505,8 +505,8 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array(end - start + 1).fill(start).map((item, index) => item + index);
 }
 
 /**
@@ -520,8 +520,8 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return [...new Set(arr)];
 }
 
 /**
@@ -554,8 +554,24 @@ function distinct(/* arr */) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const arr = [];
+
+  const countriesWithCities = array.reduce((acc, obj) => {
+    const country = keySelector(obj);
+    const city = valueSelector(obj);
+
+    if (acc[country]) {
+      acc[country].push(city);
+    } else {
+      acc[country] = [city];
+    }
+    return acc;
+  }, {});
+
+  Object.keys(countriesWithCities).map((city) => arr.push([city, countriesWithCities[city]]));
+
+  return new Map(arr);
 }
 
 /**
@@ -571,8 +587,13 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((acc, item) => {
+    const result = childrenSelector(item);
+
+    acc.push(...result);
+    return acc;
+  }, []);
 }
 
 /**
@@ -587,8 +608,17 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  const result = indexes.reduce((acc, item) => {
+    if (acc.length) {
+      acc.push(acc[acc.length - 1][item]);
+    } else {
+      acc.push(arr[item]);
+    }
+    return acc;
+  }, []);
+
+  return result[result.length - 1];
 }
 
 /**
@@ -609,8 +639,22 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length === 1) return arr;
+  const result = [];
+  if (arr.length % 2 === 0) {
+    const middle = arr.length / 2;
+    const head = arr.slice(0, middle);
+    const tail = arr.slice(middle);
+    result.push(tail, head);
+  } else {
+    const middle = Math.floor(arr.length / 2);
+    const head = arr.slice(0, middle);
+    const tail = arr.slice(middle + 1);
+    result.push(tail, arr[middle], head);
+  }
+
+  return result.flat();
 }
 
 module.exports = {
--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Parses a rfc2822 string date representation into date value
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
@@ -19,8 +18,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,10 +33,9 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
-
 
 /**
  * Returns true if specified date is leap year and false otherwise
@@ -53,10 +51,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return !(
+    date.getFullYear() % 4
+    || (date.getFullYear() % 400 && !(date.getFullYear() % 100))
+  );
 }
-
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,10 +73,22 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const result = new Date(endDate) - new Date(startDate);
+  const h = Math.trunc((result / 3600000) % 100)
+    .toString()
+    .padStart(2, '0');
+  const m = Math.trunc((result / 60000) % 60)
+    .toString()
+    .padStart(2, '0');
+  const s = Math.trunc((result / 1000) % 60)
+    .toString()
+    .padStart(2, '0');
+  const ms = Math.trunc(result % 1000)
+    .toString()
+    .padStart(3, '0');
+  return `${h}:${m}:${s}.${ms}`;
 }
-
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,10 +106,15 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  let h = new Date(date).getUTCHours();
+  h = h > 12 ? h % 12 : h;
+  const m = new Date(date).getUTCMinutes();
+  const hArrowDegree = 0.5 * (60 * h + m);
+  const mArrowDegree = 6 * m;
+  const deg = Math.abs(hArrowDegree - mArrowDegree);
+  return deg <= 360 ? (Math.PI * Math.min(360 - deg, deg)) / 180 : (Math.PI * (deg - 360)) / 180;
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
